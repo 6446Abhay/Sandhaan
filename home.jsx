2d0af5d7ca6a52786c411457ff397180e257e696
6446Abhay <inbox.abhay6446@gmail.com>
2025-09-14 00:36:05 -0700

    Create home.jsx

---
+++ home.jsx
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Navigation Menu</title>
+  <link rel="stylesheet" href="navbar.css">
+</head>
+<body>
+  <nav class="nav">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About Us</a></li>
+      <li><a href="#">About College</a></li>
+      <li><a href="#">Contact Us</a></li>
+    </ul>
+  </nav>
+</body>
+</html>
